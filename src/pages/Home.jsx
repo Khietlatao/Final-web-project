124a51686b6a0fd636ab1532bf2b7ade4ff4e393
Khietlatao <19520628@gm.uit.edu.vn>
2022-11-16 00:10:05 +0700

    lan1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+  
+const Home = () => {
+  return (
+    <div className="container">
+       <Navbar>
+
+        </Navbar>
+
+        
+        </div>
+  )
+}
+
+export default Home
